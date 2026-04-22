a6a3afa652274e77e4f5743239a4a56975125d0b
seyf-eddine NECIB <140780151+hamzouvic@users.noreply.github.com>
2026-04-22 01:42:54 -0400

    fix backend bug

--- frontend/envsubst-file.sh
+++ frontend/envsubst-file.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-: "${PORT:=8080}"
-: "${API_BASE_URL:=http://localhost:8083}"
+export PORT="${PORT:-80}"
+export API_BASE_URL="${API_BASE_URL:-http://localhost:8083}"
 
 envsubst '${API_BASE_URL} ${PORT}' < /app/nginx.conf > /etc/nginx/conf.d/default.conf
 
